407cf4f53c7dcd9ef5e2d8b68404602c4e9b7b29
Thirumal <m.thirumal@hotmail.com>
2023-02-26 09:26:30 +0530

    Authentication details. working code

--- src/main/java/in/thirumal/controller/OrderController.java
+++ src/main/java/in/thirumal/controller/OrderController.java
@@ -6,10 +6,12 @@ package in.thirumal.controller;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.thirumal.model.Order;
+import in.thirumal.security.IAuthenticationFacade;
 
 /**
  * @author Thirumal
@@ -18,9 +20,12 @@ import in.thirumal.model.Order;
 @RestController
 public class OrderController {
 
+	@Autowired
+	IAuthenticationFacade iAuthenticationFacade;
 	
 	@GetMapping("/orders")
 	public List<Order> getOrder() {
+		System.out.println(iAuthenticationFacade.getAuthentication().getName());
 		return List.of(Order.builder().id(3).itemName("Working").orderDate(OffsetDateTime.now()).build());
 	}
 	
