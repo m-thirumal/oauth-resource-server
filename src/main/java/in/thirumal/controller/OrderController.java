6d2588d95c34a396d100b7d1eee6b30e1a794c96
Thirumal <m.thirumal@hotmail.com>
2023-02-27 17:47:31 +0530

    JWT details

--- src/main/java/in/thirumal/controller/OrderController.java
+++ src/main/java/in/thirumal/controller/OrderController.java
@@ -4,9 +4,13 @@
 package in.thirumal.controller;
 
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +29,18 @@ public class OrderController {
 	
 	@GetMapping("/orders")
 	public List<Order> getOrder() {
-		System.out.println(iAuthenticationFacade.getAuthentication().getName());
 		return List.of(Order.builder().id(3).itemName("Working").orderDate(OffsetDateTime.now()).build());
+	}
+	
+	
+	/**
+	 * Accessing JWT claim details
+	 * @param jwt
+	 * @return Map
+	 */
+	@GetMapping("/token")
+	public Map<String, Jwt> getToken(@AuthenticationPrincipal Jwt jwt) {
+		return Collections.singletonMap("principal", jwt);
 	}
 	
 }
