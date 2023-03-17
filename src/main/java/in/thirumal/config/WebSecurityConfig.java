bb3f814841a333d7f44be0e90a0a52a2b45e87b5
Thirumal <m.thirumal@hotmail.com>
2023-03-17 15:46:06 +0530

    Roles in authorities

--- src/main/java/in/thirumal/config/WebSecurityConfig.java
+++ src/main/java/in/thirumal/config/WebSecurityConfig.java
@@ -3,9 +3,18 @@
  */
 package in.thirumal.config;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -26,12 +35,26 @@ public class WebSecurityConfig {
 		.authorizeHttpRequests()
 		//.antMatchers("/orders/status/check").permitAll() // Public API endpoint
 		.anyRequest()
-                .authenticated()
-	        .and()
-	        .oauth2ResourceServer()
-	        .jwt();
+                .authenticated();
+	      //  .and()
+	        //.oauth2ResourceServer()
+	        //.jwt();
  
- 
+		http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                        .jwtAuthenticationConverter(new CustomAuthenticationConverter())
+                    )
+                );
 		return http.build();
+	}
+	
+	static class CustomAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+	    public AbstractAuthenticationToken convert(Jwt jwt) {
+	    	Collection<String> authorities = jwt.getClaimAsStringList("roles");
+	    	Collection<GrantedAuthority> grantedAuthorities = authorities.stream()
+	                .map(SimpleGrantedAuthority::new)
+	                .collect(Collectors.toList());
+	    	return new JwtAuthenticationToken(jwt, grantedAuthorities);
+	    }
 	}
 }
